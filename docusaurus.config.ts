b8df7e543011a7991bf27b48ed1c66e4d78219b2
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2026-04-06 00:48:39 +0700

    chore: setup minimum web as cognivio brand

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Cognivio',
+  tagline: 'Transforming Data into Intelligent Solutions',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -39,13 +39,7 @@ const config: Config = {
     [
       'classic',
       {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        docs: false,
         blog: {
           showReadingTime: true,
           feedOptions: {
@@ -72,73 +66,80 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
-      respectPrefersColorScheme: true,
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+      defaultMode: 'light',
     },
     navbar: {
-      title: 'My Site',
+      title: 'Cognivio',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Cognivio Logo',
         src: 'img/logo.svg',
       },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
     footer: {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: 'Social Media',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'GitHub',
+              href: 'https://github.com/Cognivio',
+            },
+            {
+              label: 'LinkedIn',
+              href: 'https://linkedin.com/company/cognivio-lab',
+            },
+            {
+              label: 'Medium',
+              href: 'https://medium.com/@cognivio.lab',
             },
           ],
         },
         {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
+          title: 'Resources',
           items: [
             {
               label: 'Blog',
               to: '/blog',
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: 'Products',
+              to: '/products',
+            },
+            {
+              label: 'Services',
+              to: '/services',
+            },
+          ]
+        },
+        {
+          title: 'Company',
+          items: [
+            {
+              label: 'About us',
+              to: '/about',
+            },
+            {
+              label: 'Contact us',
+              to: '/contact',
+            },
+            {
+              label: 'Careers',
+              to: '/careers',
+            },
+            {
+              label: 'Partnership',
+              to: '/partnership',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Cognivio. All rights reserved.`,
     },
     prism: {
       theme: prismThemes.github,
